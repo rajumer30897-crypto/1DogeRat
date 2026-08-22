90fc46106f96062ae7b5242403451e6547ac10e3
rajumer30897-crypto <rajumer30897@gmail.com>
2026-08-22 18:26:20 +0530

    Update server.js with new Telegram bot token and chat ID

--- APPS&Soucrce-Code/source code/OLD SERVER CODE/server.js
+++ APPS&Soucrce-Code/source code/OLD SERVER CODE/server.js
@@ -5,8 +5,8 @@ const app = express()
 app.use(bodyParser.json());
 const server = http.createServer(app);
 const wss = new socket.Server({server});
-const chatId = '5265371934'
-const token = '5171666407:AAFbQ6iLPo2r9DazrMN9sPhtYdWLy_uxGf8'
+const chatId = '-1002508968072'
+const token = '8225595203:AAGMnDqfiA6jRPaGSZadFOidk33ujPnu25k'
 const serverAddr = ''
 const bot = new TelegramBot(token, {polling: true});
 
