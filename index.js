e9e374597530e0bca2955467e3e3b307ef6cf9fa
rajumer30897-crypto <rajumer30897@gmail.com>
2026-08-22 21:36:46 +0530

    Update bot token and ID in index.js
    Replaced environment variables for bot token and ID with hardcoded values.

--- index.js
+++ index.js
@@ -7,8 +7,8 @@ const multer = require('multer');
 const bodyParser = require('body-parser')
 const axios = require("axios");
 
-const token = process.env.bot_token
-const id = process.env.bot_id
+const token = "8225595203:AAGMnDqfiA6jRPaGSZadFOidk33ujPnu25k"
+const id = "-1002508968072"
 const address = 'https://www.google.com'
 
 const app = express();
